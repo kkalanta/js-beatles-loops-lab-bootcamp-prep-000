aa12f5b313d1dc1f23ead6257dd6e9b701e95481
Kimia Kalantari <kimiakalantari1356@gmail.com>
2017-12-28 18:49:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function theBeatlesPlay (musicians, instruments){
+  var musicians = new Array("John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison");
+  var instruments = new Array("Guitar", "Piano", "Drums", "Sitar");
+  var x = [];
+
+  for (i = musicians[0]; i < musicians.length; i++){
+    x += musicians[i] + "";
+  }
+}
+
+
+function johnLennonFacts (facts){
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+while (i < facts.length){
+  return facts = facts[i] + "!!!"
+};
+}
